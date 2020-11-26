7bb9568814aba4b867689bb0f9a74911cdda640f
sgmsgood <sgms_good@naver.com>
2020-11-26 16:45:30 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-t4uon3-xgtp95
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-t4uon3-xgtp95)
---
+++ index.js
@@ -0,0 +1,70 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "antd/dist/antd.css";
+import "./index.css";
+import moment from "moment";
+import { DatePicker, Space } from "antd";
+
+const { RangePicker } = DatePicker; 
+
+function range(start, end) {
+  const result = [];
+  for (let i = start; i < end; i++) {
+    result.push(i);
+  }
+  return result;
+}
+
+function disabledDate(current) {
+  // Can not select days before today and today
+  return current && current < moment().startOf("day");
+}
+
+const disabledDateTime = () => {
+  return {
+    disabledHours: () => range(0, 24),
+    disabledMinutes: () => range(30, 60),
+    disabledSeconds: () => [55, 56]
+  };
+};
+
+function disabledRangeTime(_, type) {
+  if (type === "start") {
+    return {
+      disabledHours: () => range(0, 60).splice(4, 20),
+      disabledMinutes: () => range(30, 60),
+      disabledSeconds: () => [55, 56]
+    };
+  }
+  return {
+    disabledHours: () => range(0, 60).splice(20, 4),
+    disabledMinutes: () => range(0, 31),
+    disabledSeconds: () => [55, 56]
+  };
+}
+
+ReactDOM.render(
+  <Space direction="vertical" size={12}>
+    <DatePicker
+      format="YYYY-MM-DD HH:mm:ss"
+      disabledDate={disabledDate}
+      disabledTime={disabledDateTime}
+      showTime={{ defaultValue: moment("00:00:00", "HH:mm:ss") }}
+    />
+    <DatePicker picker="month" disabledDate={disabledDate} />
+    <RangePicker disabledDate={disabledDate({ type: "end" })} />
+    <RangePicker
+      disabledDate={disabledDate}
+      disabledTime={disabledRangeTime}
+      showTime={{
+        hideDisabledOptions: true,
+        defaultValue: [
+          moment("00:00:00", "HH:mm:ss"),
+          moment("11:59:59", "HH:mm:ss")
+        ]
+      }}
+      format="YYYY/MM/DD HH:mm:ss"
+    />
+  </Space>,
+  document.getElementById("container")
+);
